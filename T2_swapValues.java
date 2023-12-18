22ae35da567585fec56eacecba6afadcde695071
ivan3449 <48065930+ivan3449@users.noreply.github.com>
2023-12-18 21:46:26 +0200

    Add files via upload

---
+++ T2_swapValues.java
@@ -0,0 +1,23 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class T2_swapValues {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter A: ");
+        int a = sc.nextInt();
+        System.out.println("Enter B: ");
+        int b = sc.nextInt();
+
+        System.out.println("A is: " + a + " and B is: " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("A now is: " + a + " and B now is: " + b);
+
+    }
+}
