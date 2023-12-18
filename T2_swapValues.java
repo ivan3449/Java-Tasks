a6c8872886909d7e2c351226dcca08143dca3411
ivan3449 <48065930+ivan3449@users.noreply.github.com>
2023-12-18 21:48:12 +0200

    T2_swapValues.java

--- T2_swapValues.java
+++ T2_swapValues.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class T2_swapValues {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter A: ");
