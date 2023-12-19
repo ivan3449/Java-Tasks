b137ae1ed5b53706dfc4fd729a745060dc8a0de4
ivan3449 <48065930+ivan3449@users.noreply.github.com>
2023-12-19 14:37:46 +0200

    Add files via upload

---
+++ AT1_loopNumbers.java
@@ -0,0 +1,10 @@
+package Lesson1;
+
+public class AT1_loopNumbers {
+    public static void main(String[] args) {
+        //Print numbers [1..100]
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
